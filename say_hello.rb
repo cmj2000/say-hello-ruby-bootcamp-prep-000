2a57164ceb06ed3c3403cdff79450f23d63dfabd
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-22 01:38:04 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 
-def say_hello(phrase = "Hello", name = "Ruby Programmer")
-  puts(phrase+ " " + name + "!")
+def say_hello name = "Ruby Programmer")
+  puts("Hello" + name + "!")
 end
